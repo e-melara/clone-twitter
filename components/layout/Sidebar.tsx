ff1921502805e5e698c77b4b5ab10844fbd6f5ca
Consultoria 01 SV <consultoria01sv@gmail.com>
2024-03-07 22:58:19 -0600

    Sidebar

---
+++ components/layout/Sidebar.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+import { FaUser } from "react-icons/fa";
+import { BiLogOut } from "react-icons/bi";
+import { BsHouseFill, BsBellFill } from "react-icons/bs";
+
+import SidebarItem from "./SidebarItem";
+import SidebarLogo from "./SidebarLogo";
+import SidebarTweetButton from "./SidebarTweetButton";
+
+const Sidebar: React.FC = () => {
+  const items = [
+    {
+      label: "Home",
+      href: "/",
+      icon: BsHouseFill,
+    },
+    {
+      label: "Notificaciones",
+      href: "/notifications",
+      icon: BsBellFill,
+    },
+    {
+      label: "Profile",
+      href: "/users/123",
+      icon: FaUser,
+    },
+  ];
+  return (
+    <div className="col-span-1 h-full pr-4 md:pr-6">
+      <div className="flex flex-col items-end">
+        <div className="space-y-2 lg:w-[230px]">
+          <SidebarLogo />
+          {items.map((item, index) => (
+            <SidebarItem
+              key={index}
+              label={item.label}
+              href={item.href}
+              icon={item.icon}
+            />
+          ))}
+          <SidebarItem onClick={() => {}} icon={BiLogOut} label="Logout"  />
+          <SidebarTweetButton />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ components/layout/SidebarLogo.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useRouter } from "next/router";
+
+import { BsTwitter } from "react-icons/bs";
+
+const SidebarLogo: React.FC = () => {
+  const router = useRouter();
+  return (
+    <div 
+      onClick={() => router.push("/")}
+      className="rounded-full h-14 w-14 p-4 flex items-center justify-center hover:bg-blue-300 hover:bg-opacity-10 transition cursor-pointer">
+      <BsTwitter className="text-blue-500" size={30} />
+    </div>
+  );
+};
+
+export default SidebarLogo;
