cf35dd653cc5c68de95bf8612bf4c236df2058b8
Consultoria 01 SV <consultoria01sv@gmail.com>
2024-03-08 00:22:17 -0600

    modal login

--- components/layout/SidebarTweetButton.tsx
+++ components/layout/SidebarTweetButton.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useRouter } from 'next/router';
 
 import { FaFeather } from 'react-icons/fa';
+import useLoginModal from '@/hooks/useLoginModal';
 
 interface SidebarTweetButtonProps {
 }
 
 const SidebarTweetButton: React.FC<SidebarTweetButtonProps> = ({  }) => {
     const router = useRouter();
+    const loginModal = useLoginModal();
+
+    const onClick = useCallback(() => {
+        loginModal.onOpen();
+    }, [loginModal]);
     return (
-        <div onClick={() => router.push("/") }>
+        <div>
             <div className="mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex items-center justify-center bg-sky-500 hover:bg-opacity-80 transition cursor-pointer">
                 <FaFeather color='white' size={24} />
             </div>
