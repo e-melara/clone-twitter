65ac6b3b404523d84b6921349452e5f63be7f129
Consultoria 01 SV <consultoria01sv@gmail.com>
2024-03-07 23:47:10 -0600

    Component Input

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
-import Modal from "@/components/Modal";
 import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
