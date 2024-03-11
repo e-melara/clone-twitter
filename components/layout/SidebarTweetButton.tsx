94e13d19f0396edd820d2398cb7856d00176115d
Edwin Melara <edwin.melara.landaverde@gmail.com>
2024-03-11 14:10:30 -0600

    Primsa JS

--- components/layout/SidebarTweetButton.tsx
+++ components/layout/SidebarTweetButton.tsx
@@ -1,5 +1,4 @@
 import React, { useCallback } from 'react';
-import { useRouter } from 'next/router';
 
 import { FaFeather } from 'react-icons/fa';
 import useLoginModal from '@/hooks/useLoginModal';
@@ -8,14 +7,13 @@ interface SidebarTweetButtonProps {
 }
 
 const SidebarTweetButton: React.FC<SidebarTweetButtonProps> = ({  }) => {
-    const router = useRouter();
     const loginModal = useLoginModal();
 
     const onClick = useCallback(() => {
         loginModal.onOpen();
     }, [loginModal]);
     return (
-        <div>
+        <div onClick={onClick}>
             <div className="mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex items-center justify-center bg-sky-500 hover:bg-opacity-80 transition cursor-pointer">
                 <FaFeather color='white' size={24} />
             </div>
